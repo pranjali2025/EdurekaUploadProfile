c9a674097ff43802471725592f43a301a90bf4b1
Pranjali <48191458+pranjali2025@users.noreply.github.com>
2019-10-21 16:17:50 -0400

    Update DataProviders.java

--- src/test/java/co/edureka/generics/DataProviders.java
+++ src/test/java/co/edureka/generics/DataProviders.java
@@ -9,10 +9,8 @@ public class DataProviders {
 	public String[][]  getData()
 	{
 		String credentials[][] = new String[1][2];
-		credentials[0][0]="pranjali.yadav2025@gmail.com";
-		credentials[0][1]="pranjali.yadav2025@3073";
-		//credentials[1][0]="pranjali.yadav2025@gmail.com";
-		//credentials[1][1]="pranjali.yadav2025@3073";
+		credentials[0][0]="@gmail.com";
+		credentials[0][1]="abcd";
 		
 		return credentials;
 		
